dd73e5f7deb43eba02b587fa5d5fb5498d861b4d
Moises <moisesdavidlara1@gmail.com>
2025-04-11 17:40:54 -0400

    prepare to integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,17 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+
 // https://astro.build/config
 export default defineConfig({
-
   experimental: {
     svg: true,
   },
-  
+
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [],
 });
