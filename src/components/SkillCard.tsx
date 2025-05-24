63a06e3a291ad0cae3312f884c729549c5421898
Moises Lara <Moisesdavidlara1@gamil.com>
2025-05-24 17:25:25 -0400

    Actualizar componente SkillCard: ajustar el ancho de la tarjeta para mejorar la visualización en diferentes tamaños de pantalla.

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -12,7 +12,7 @@ type SkillCardProps = {
 
 export const SkillCard = ({ name, image }: SkillCardProps) => (
     <div
-        className={`flex group hover:scale-110 transition-transform relative z-10 logo flex-col items-center p-1 justify-center w-20 h-auto`}
+        className={`flex group hover:scale-110 transition-transform relative z-10 logo flex-col items-center p-1 justify-center w-14 sm:w-20 h-auto`}
    
 
 
