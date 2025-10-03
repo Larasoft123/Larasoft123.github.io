97a5d3cc71bcc98459f32d876cce906bdcd47bc1
Moises Lara <Moisesdavidlara1@gamil.com>
2025-10-03 10:26:20 -0400

    Refactor Skills section to use Container component; update global CSS for theme colors and animations; streamline tsconfig.json paths and options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
-
-import vercel from "@astrojs/vercel";
-
-
-import react from "@astrojs/react";
-
 
 // https://astro.build/config
 export default defineConfig({
-  // env: {
-  //   schema: {
-  //     API_SECRET_KEY: envField.string({context: "server",  access:"secret", default:""  }),
-  //   }
-
-  // },
-
-
-
-
   vite: {
-    
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
-  adapter: vercel(),
+  integrations: [],
 });
