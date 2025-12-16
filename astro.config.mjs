e5f708d0f45a8a1926916d59de78fdf46d8e7201
Moises Lara <moisesdavidlara1@gmail.com>
2025-12-16 15:27:18 -0400

    feat(I18N): :sparkles: add I18N config
    configure i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+
+  },
   vite: {
     plugins: [tailwindcss()],
   },
