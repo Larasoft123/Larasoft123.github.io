0966dc0019cbe4f14a21ee517e6bb8e4ff7b5490
Moises Lara <moisesdavidlara1@gmail.com>
2025-12-18 16:09:10 -0400

    fix(upgarade): :fire: remove unnecesary thing for new types
    adapt data to new types

--- src/consts/Proyects.ts
+++ src/consts/Proyects.ts
@@ -1,21 +1,5 @@
-import { type Proyect } from "@/types/proyectType";
-import angular from "@/assets/Logos/angular.svg";
-import react from "@/assets/Logos/react_wordmark_dark.svg";
-import vue from "@/assets/Logos/vue.svg";
-import node from "@/assets/Logos/nodejs.svg";
-import javascript from "@/assets/Logos/javascript.svg";
-import tailwindcss from "@/assets/Logos/tailwindcss-wordmark-dark.svg";
-import typescript from "@/assets/Logos/typescript.svg";
-import sass from "@/assets/Logos/sass.svg";
-import bootstrap from "@/assets/Logos/bootstrap.svg";
-// import astro from "@/assets/Logos/svg-icon-light-gradient.svg";
-import rxjs from "@/assets/Logos/rxjs.svg";
-import html5 from "@/assets/Logos/html5.svg";
-import Git from "@/assets/Logos/Git.svg";
-import Vitejs from "@/assets/Logos/vitejs.svg";
-import Python from "@/assets/Logos/python.svg";
-import Css from "@/assets/Logos/Css.svg";
-import NextJs from "@/assets/Logos/NextJs.svg";
+import { type Project } from "@/types/types";
+
 
 
 
@@ -23,109 +7,72 @@ import NextJs from "@/assets/Logos/NextJs.svg";
 const tags = {
   js: {
     name: "javascript",
-    icon: {
-      logo: javascript,
-    },
   },
   css: {
     name: "CSS",
-    icon: {
-      logo: Css,
-    },
+
   },
   html: {
     name: "HTML",
-    icon: {
-      logo: html5,
-    },
+
   },
   ts: {
     name: "typescript",
-    icon: {
-      logo: typescript,
-    },
+
   },
   react: {
     name: "react",
-    icon: {
-      logo: react,
-    },
+
   },
   next: {
     name: "next",
-    icon: {
-      logo: NextJs,
-    },
+
   },
   tailwind: {
     name: "tailwind",
-    icon: {
-      logo: tailwindcss,
-    },
+
   },
-  // astro: {
-  //   name: "astro",
-  //   icon: {
-  //     logo: astro,
-  //   },
-  // },
+
   vite: {
     name: "vite",
-    icon: {
-      logo: Vitejs,
-    },
+
   },
   git: {
     name: "git",
-    icon: {
-      logo: Git,
-    },
+
   },
   angular: {
     name: "angular",
-    icon: {
-      logo: angular,
-    },
+
   },
   sass: {
     name: "sass",
-    icon: {
-      logo: sass,
-    },
+
   },
   bootstrap: {
     name: "bootstrap",
-    icon: {
-      logo: bootstrap,
-    },
+
   },
-  rxjs: {
-    name: "rxjs",
-    icon: {
-      logo: rxjs,
-    },
-  },
+
   node: {
     name: "node",
-    icon: {
-      logo: node,
-    },
   },
 };
 
-export const ProyectsList: Proyect[] = [
+export const ProyectsList: Project[] = [
   {
-    name: "Juego de Memoria",
+    id: 1,
+    title: "Juego de Memoria",
     description:
       "Juego de memoria hecho con JavaScript vanila, !Porque me gusta sufrir!",
     image: "../../images/proyects/Juego-memoria.webp",
     link: "https://larasoft123.github.io/juego-memoria/",
     tags: [tags.js, tags.css, tags.html],
-    github: "https://github.com/Larasoft123/juego-memoria",
-    date: "July 2024",
+    github: "https://github.com/Larasoft123/juego-memoria"
   },
   {
-    name: "App de listas",
+    id: 2,
+    title: "App de listas",
     description: "App de listas hecho con Angular, Tailwind, typescript y la api de Gemini .",
     image: "../../images/proyects/gemini-app.webp",
     link: "https://larasoft123.github.io/app-listas-gemini/",
@@ -136,13 +83,14 @@ export const ProyectsList: Proyect[] = [
       tags.angular,
       tags.tailwind,
       tags.sass,
-  
+
     ],
-    github: "https://github.com/Larasoft123/app-listas-gemini",
-    date: "Jan 2025",
+    github: "https://github.com/Larasoft123/app-listas-gemini"
+
   },
   {
-    name: "App de países",
+    id: 3,
+    title: "App de países",
     description: "App de países hecho con React Js, Tailwind, typescript y la api REST Countries",
     image: "../../images/proyects/Countries-app.webp",
     link: "https://countries-app-pi-black.vercel.app/",
@@ -153,14 +101,12 @@ export const ProyectsList: Proyect[] = [
       tags.react,
       tags.tailwind,
       tags.vite
-
-  
     ],
-    github: "https://github.com/Larasoft123/app-countries", 
-    date: "Jan 2025",
+    github: "https://github.com/Larasoft123/app-countries",
   },
   {
-    name: "La velada del año V",
+    id: 4,
+    title: "La velada del año V",
     description: "Participación en el proyecto de 'La Velada del Año V' liderado por la comunidad de Midudev,  el evento más esperado organizado por Ibai Llanos.",
     image: "../../images/proyects/Velada.webp",
     link: "https://la-velada-web-oficial-h4wiyiv2f-midudev-pro.vercel.app/",
@@ -168,15 +114,14 @@ export const ProyectsList: Proyect[] = [
       tags.ts,
       tags.css,
       tags.html,
-      // tags.astro,
       tags.js,
       tags.tailwind,
 
 
-  
+
     ],
-    github: "https://github.com/midudev/la-velada-web-oficial", 
-    date: "April 2025",
+    github: "https://github.com/midudev/la-velada-web-oficial",
+
   }
 
 ];
--- src/consts/tecnologies.ts
+++ src/consts/tecnologies.ts
@@ -1,188 +1,73 @@
-import type { Tecnologies } from "../types/tecnologiesType";
-import angular from "@/assets/Logos/angular.svg?url";
-import react from "@/assets/Logos/react_wordmark_dark.svg?url";
-import vue from "@/assets/Logos/vue.svg?url";
-import node from "@/assets/Logos/nodejs.svg?url";
-import javascript from "@/assets/Logos/javascript.svg?url";
-import tailwindcss from "@/assets/Logos/tailwindcss-wordmark-dark.svg?url";
-import typescript from "@/assets/Logos/typescript.svg?url";
-import sass from "@/assets/Logos/sass.svg?url";
-import bootstrap from "@/assets/Logos/bootstrap.svg?url";
-// import astro from "@/assets/Logos/svg?url-icon-light-gradient.svg?url";
-import rxjs from "@/assets/Logos/rxjs.svg?url";
-import html5 from "@/assets/Logos/html5.svg?url";
-import vercel from "@/assets/Logos/vercel_wordmark_dark.svg?url";
-import midudev from "@/assets/Logos/midudev.svg?url";
-import Git from "@/assets/Logos/Git.svg?url";
-import Vitejs from "@/assets/Logos/vitejs.svg?url";
-// import Python from "@/assets/Logos/python.svg?url";
-import NextJs from "@/assets/Logos/NextJs.svg?url";
-import Css from "@/assets/Logos/Css.svg?url";
 
-export const tecnologies: Tecnologies[] = [
-    {
-      name: "Angular",
-      image: {
-        logo: angular,
-        width: 100,
-        height: 100,
-        className: "w-10 sm:w-12 xl:w-14",
-      },
-    },
+
+
+export const tecnologies = [
+  {
+    name: "Angular",
+
+  },
   {
     name: "React",
-    image: {
-      logo: react,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-20",
-    },
+
   },
   {
     name: "Vue",
-    image: {
-      logo: vue,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
+
   },
   {
     name: "Git",
-    image: {
-      logo: Git,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
+
   },
   {
     name: "JavaScript",
-    image: {
-      logo: javascript,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
+
   },
   {
     name: "TailwindCSS",
-    image: {
-      logo: tailwindcss,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-16",
-    },
+
   },
   {
     name: "Node.js",
-    image: {
-      logo: node,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-20",
-    },
+
   },
   {
     name: "TypeScript",
-    image: {
-      logo: typescript,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
+
   },
   {
     name: "NextJs",
-    image: {
-      logo: NextJs,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
+
   },
   {
     name: "Sass",
-    image: {
-      logo: sass,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
+
   },
   {
     name: "Bootstrap",
-    image: {
-      logo: bootstrap,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
+
   },
-  // {
-  //   name: "Astro",
-  //   image: {
-  //     logo: astro,
-  //     width: 100,
-  //     height: 100,
-  //     className: "w-10 sm:w-12 xl:w-14",
-  //   },
-  // },
+
   {
     name: "RxJS",
-    image: {
-      logo: rxjs,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
+
   },
   {
     name: "HTML5",
-    image: {
-      logo: html5,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
+
   },
 
   {
     name: "CSS",
-    image: {
-      logo: Css,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
+
   },
 
 
   {
     name: "Midudev",
-    image: {
-      logo: midudev,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
+
   },
   {
     name: "Vitejs",
-    image: {
-      logo: Vitejs,
-      width: 100,
-      height: 100,
-      className: "w-10 sm:w-12 xl:w-14",
-    },
-  },
-  // {
-  //   name: "Python",
-  //   image: {
-  //     logo: Python,
-  //     width: 100,
-  //     height: 100,
-  //     className: "w-10 sm:w-12 xl:w-14",
-  //   },
-  // },
+
+  }
 ];
